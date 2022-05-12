a09565560950fd50cf0f52bc377c21bd37712374
DNIIBOY <daniel.nettelfield@gmail.com>
2022-05-12 16:19:15 +0200

    big msg

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,19 @@
 <?php
 
     if(isset($_POST["submit"])){
-        echo($_POST["message"]);
 
         $mailTo = "daniel@nettelfield.com";  // Min email addresse
 
         // Hent kundedata
+        $name = $_POST["name"];
+        $fromEmail = $_POST["email"];
+        $subject = $_POST["package"];
+        $message = $_POST["message"];
+
+        $fullMessage = "Navn: " . $name . "\n"
+        . "Bruger email: " . $fromEmail . "\n"
+        . "\n\n" . "Besked: " . "\n" . $message;
+        echo($fullMessage);
     }
 
 ?>
