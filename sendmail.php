402bcd04e92223ada6b6994a95391eb40d4003a8
DNIIBOY <daniel@nettelfield.com>
2022-05-12 11:40:51 +0200

    Bigger sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,42 @@
 <?php
-if($_POST["message"]) {
-	mail("daniel@nettelfield.com", "Here is the subject line",
-	$_POST["message"]. "From: daniel.nettelfield@gmail.com");
-?>
+    if(isset($_POST['submit'])) {
 
+        $mailto = "daniel@nettelfield.com";  // Min mail addresse
+
+        // Hent kundedata
+        $name = $_POST['name']; // Hent kundens navn
+        $fromEmail = $_POST['email']; // Hent kundens email
+        $subject = $_POST['package']; // Hent hvilken pakke de vil have
+
+        $subject2 = "Godkendelse: Din besked er modtaget | Daniel Nettelfield"; // Godkendelse til kunden
+
+        // Email til mig
+        $message = "Navn: " . $name . "\n\n"
+        . "Besked: " . "\n" . $_POST['message'];
+
+        // Email som kunden modtager
+        $message2 = "Dear" . $name . "\n"
+        . "Tak for at kontakte mig! Jeg vil give dig en respons hurtigts muligt." . "\n\n"
+        . "Din besked er følgende:" . "\n" . $_POST['message'] . "\n\n"
+        . "Med venlig hilsen," . "\n" . "Daniel Nettelfield";
+
+        // Email headers
+        $headers = "Fra: " . $fromEmail; // Kunde email, som jeg modtager
+        $headers2 = "Til: " . $mailto; // Email til kunden
+
+        //PHP mailer function
+
+        $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+        $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+
+        //Checking if Mails sent successfully
+
+        if ($result1 && $result2) {
+            $success = "Your Message was sent Successfully!";
+        } else {
+            $failed = "Sorry! Message was not sent, Try again Later.";
+        }
+
+}
+
+?>
